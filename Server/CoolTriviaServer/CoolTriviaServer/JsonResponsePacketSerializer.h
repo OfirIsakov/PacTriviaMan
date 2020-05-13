0f9802ecc796f6eed48d13160d3ad643fed2653e
Ron Hugi <ron.hugi1@gmail.com>
2020-05-13 18:45:15 +0300

    Added The Class (Json Serializer)

---
+++ Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.h
@@ -0,0 +1,16 @@
+#pragma once
+class JsonResponsePacketSerializer
+{
+	
+
+};
+
+typedef struct LoginResponse
+{
+	unsigned int status;
+} LoginResponse;
+
+typedef struct SignupResponse
+{
+	unsigned int status;
+} SignupResponse;

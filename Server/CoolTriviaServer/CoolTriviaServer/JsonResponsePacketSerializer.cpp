d5099b0081a6afae9dd98c0b0f9fee18557c4e7f
Ron Hugi <ron.hugi1@gmail.com>
2020-05-13 23:40:09 +0300

    Start To Work On Serialize Functions

--- Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.cpp
+++ Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.cpp
@@ -1,1 +1,35 @@
 #include "JsonResponsePacketSerializer.h"
+
+
+unsigned char* JsonResponsePacketSerializer::serializeLoginResponse(LoginResponse res)
+{
+	json loginJson = json::parse("{\"status\":" + std::to_string(res.status) + "}");
+	unsigned char* buffer[loginJson.dump.length() + 5];
+	buffer[0] = (char)login;
+
+	return buffer;
+}
+
+unsigned char* JsonResponsePacketSerializer::serializeSignUpResponse(SignupResponse res)
+{
+	json signupJson = json::parse("{\"status\":" + std::to_string(res.status) + "}");
+	int jsonStringLen = signupJson.dump.length();
+	unsigned char* buffer[signupJson.dump.length() + 5];
+	
+	buffer[0] = (char)signup;
+	// helper
+	char arr[4];
+
+	buffer[1] = (signupJson.dump.length() >> 24) & 0xFF;
+	buffer[2] = (signupJson.dump.length() >> 16) & 0xFF;
+	buffer[3] = (signupJson.dump.length() >> 8) & 0xFF;
+	buffer[4] = signupJson.dump.length() & 0xFF;
+
+
+	return buffer;
+}
+
+unsigned char* serializeErrorResponse(ErrorResponse res)
+{
+
+}
--- Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.h
+++ Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.h
@@ -1,9 +1,10 @@
 #pragma once
-class JsonResponsePacketSerializer
-{
-	
 
-};
+#include <string>
+#include "Helper.h"
+#include "consts.h"
+
+using std::string;
 
 typedef struct LoginResponse
 {
@@ -14,3 +15,19 @@ typedef struct SignupResponse
 {
 	unsigned int status;
 } SignupResponse;
+
+typedef struct ErrorResponse
+{
+	string message;
+} ErrorResponse;
+
+
+class JsonResponsePacketSerializer
+{
+public:
+
+	static unsigned char* serializeLoginResponse(LoginResponse res);
+	static unsigned char* serializeSignUpResponse(SignupResponse res);
+	static unsigned char* serializeErrorResponse(ErrorResponse res);
+
+};

8e28761025cdc31ebc22e1c5cd3692a830beadfb
Ron Hugi <ron.hugi1@gmail.com>
2020-05-14 20:07:02 +0300

    Done serializer

--- Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.cpp
+++ Server/CoolTriviaServer/CoolTriviaServer/JsonResponsePacketSerializer.cpp
@@ -1,35 +1,31 @@
 #include "JsonResponsePacketSerializer.h"
 
-
+// Function will serialize the login message
 unsigned char* JsonResponsePacketSerializer::serializeLoginResponse(LoginResponse res)
 {
 	json loginJson = json::parse("{\"status\":" + std::to_string(res.status) + "}");
 	unsigned char* buffer[loginJson.dump.length() + 5];
-	buffer[0] = (char)login;
 
+	buffer = (char)login + Helper::convertIntToFourBytes(loginJson.dump.length()) + loginJson.dump;
 	return buffer;
 }
 
+// Function will serialize the signup message
 unsigned char* JsonResponsePacketSerializer::serializeSignUpResponse(SignupResponse res)
 {
 	json signupJson = json::parse("{\"status\":" + std::to_string(res.status) + "}");
-	int jsonStringLen = signupJson.dump.length();
 	unsigned char* buffer[signupJson.dump.length() + 5];
-	
-	buffer[0] = (char)signup;
-	// helper
-	char arr[4];
 
-	buffer[1] = (signupJson.dump.length() >> 24) & 0xFF;
-	buffer[2] = (signupJson.dump.length() >> 16) & 0xFF;
-	buffer[3] = (signupJson.dump.length() >> 8) & 0xFF;
-	buffer[4] = signupJson.dump.length() & 0xFF;
-
-
+	buffer = (char)signup + Helper::convertIntToFourBytes(signupJson.dump.length()) + signupJson.dump;
 	return buffer;
 }
 
+// Function will serialize the error message
 unsigned char* serializeErrorResponse(ErrorResponse res)
 {
+	json errorJson = json::parse("{\"message\":" + res.message + "}");
+	unsigned char* buffer[errorJson.dump.length() + 5];
 
+	buffer = (char)error + Helper::convertIntToFourBytes(errorJson.dump.length()) + errorJson.dump;
+	return buffer;
 }

e7aa4f8ea61de5946aa5c9139d4cd812c3ce34ea
Ron Hugi <ron.hugi1@gmail.com>
2020-05-22 00:49:43 +0300

    Added LoginManager Class With All The Functions

---
+++ Server/CoolTriviaServer/CoolTriviaServer/LoginManager.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SqliteDataBase.h"
+#include "LoggedUser.h"
+#include "UserIsntExistException.h"
+#include "PasswordIsntMatchException.h"
+
+class LoginManager
+{
+public:
+	
+	void signup(string username, string password, string mail);
+	void login(string username, string password);
+	void logout(string username);
+
+private:
+
+	IDatabase* m_database;
+	vector<LoggedUser> m_loggedUsers;
+};
+
